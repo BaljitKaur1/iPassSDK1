8d6bcbca62b78fd7c467d06132c9d04cea1f8e7c
Baljit <baljitkaur@csgroupchd.com>
2023-02-07 17:27:45 +0530

    Added files

---
+++ iPassSDK1/Sources/ApiStore.swift
@@ -0,0 +1,34 @@
+//
+//  ApiStore.swift
+//  iPassSDK1
+//
+//  Created by CS on 07/02/23.
+//
+
+import UIKit
+
+class ApiStore: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        test()
+        // Do any additional setup after loading the view.
+    }
+    
+    func test() {
+        print("test=-=-=")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
