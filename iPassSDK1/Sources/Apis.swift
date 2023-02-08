11dea4df6dcda8a1cd38db079be9741f9d31792e
BaljitKaur1 <baljitkaur@csgroupchd.com>
2023-02-08 20:01:03 +0530

    Added api file

---
+++ iPassSDK1/Sources/Apis.swift
@@ -0,0 +1,17 @@
+//
+//  Apis.swift
+//  iPassSDK1
+//
+//  Created by Mobile on 08/02/23.
+//
+
+import UIKit
+
+class Apis: NSObject {
+    
+    static let shared = Apis()
+    
+    func testDemo() {
+        print("=-=-=-===")
+    }
+}
