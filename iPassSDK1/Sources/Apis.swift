3c561374c849b41cf7de1518bb6fd6cda0ffd3ea
BaljitKaur1 <baljitkaur@csgroupchd.com>
2023-03-22 15:13:13 +0530

    Added alamofire method

--- iPassSDK1/Sources/Apis.swift
+++ iPassSDK1/Sources/Apis.swift
@@ -14,4 +14,18 @@ open class Apis: NSObject {
     open func testDemo() {
         print("=-=-=-===testDemo")
     }
+    
+    open func getAlamofire() {
+        AF.request("https://mbd.cookcountysupernetwork.com/ap/swift_math_get.asp", method: .post,  parameters: nil, encoding: JSONEncoding.default)
+            .responseJSON { response in
+                switch response.result {
+                case .success(let value):
+                    if let json = value as? [String: Any] {
+                        print(json["Result"] as? Int)
+                    }
+                case .failure(let error):
+                    print(error)
+                }
+            }
+    }
 }
